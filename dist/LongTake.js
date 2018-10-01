f42d8e8ba685eed8efd0706660199fa6ad82ee73
zhihao <zhihao@softchef.com>
2018-10-01 16:43:00 +0800

    update

--- dist/LongTake.js
+++ dist/LongTake.js
@@ -111,7 +111,7 @@ class RenderBuffer extends ModuleBase {
         this.width = main.width;
         this.height = main.height;
         this.canvas = document.createElement('canvas');
-        this.context = this.canvas.getContext('2d', { alpha : false });
+        this.context = this.canvas.getContext('2d');
         this.camera = main.camera;
         this.resize( main.width, main.height );
     }
