74ce24332581062f0351d25f506ffe2be0d73075
zhihao <zhihao@softchef.com>
2018-11-21 01:29:02 +0800

    fix bug

--- dist/LongTake.js
+++ dist/LongTake.js
@@ -502,8 +502,7 @@ class Bitmap extends ModuleBase {
 
     constructor( width = 100, height = 100, element, context = '2d' ){
         super("Bitmap");
-        // this.offscreenCanvasSupport = !!self.OffscreenCanvas;
-        this.offscreenCanvasSupport = false;
+        this.offscreenCanvasSupport = !!self.OffscreenCanvas;
         this.canvas = element || this.offscreenCanvasSupport ? new OffscreenCanvas(width, height) : document.createElement('canvas');
         this.context = this.canvas.getContext(context);
         this.cache = false;
@@ -575,6 +574,7 @@ class Bitmap extends ModuleBase {
     cacheImageBitmap(){
         if( this.offscreenCanvasSupport ){
             this.imgBitmap = this.canvas.transferToImageBitmap();
+            this.imgBitmap.close();
         }else{
             let img = new Image();
             img.onload = ()=>{ this.imgBitmap = img }
